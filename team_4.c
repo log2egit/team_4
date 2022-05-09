7d66ccbcc773819526a552e24afd356067a7eb96
log2egit <95142616+log2egit@users.noreply.github.com>
2022-05-09 18:04:10 +0300

    Update team_4.c

--- team_4.c
+++ team_4.c
@@ -67,7 +67,7 @@ int check_data(data *cur_data) {
     return 0;
 }
 
-int sortion_test(void (*func)(data*, struct step_arr*), struct step_arr *(*step_func)(data*), long long start, long long stop, long long step, int repeat, char* file_name) {
+int sortion_test(void (*func)(data*, struct step_arr*), struct step_arr *(*step_func)(data*), int start, int stop, int step, int repeat, char* file_name) {
     struct res_collection *cur_res_col = (struct res_collection*)malloc(sizeof(struct res_collection));
     if (cur_res_col == NULL) exit(-2);
     cur_res_col->res_arr = (res*)malloc(((stop-start)/step+1)*sizeof(res));
@@ -76,6 +76,7 @@ int sortion_test(void (*func)(data*, struct step_arr*), struct step_arr *(*step_
     for (int r = 0; r<repeat; r++) {
         cur_res_col->n = 0;
         for (int i = start; i<=stop; i+=step) {
+            printf("%i/%i - %i/%i\n", r, repeat, i, stop);
             data *cur_data = gen_data(i);
             struct step_arr *cur_step_arr = step_func(cur_data);
             cur_time = get_time(func, cur_data, cur_step_arr);
@@ -102,20 +103,20 @@ int sortion_test(void (*func)(data*, struct step_arr*), struct step_arr *(*step_
 
 int main() {
     int res = 0;
-//    res += sortion_test(classic_insertion_sort, not_req, 0, 150000, 5000, 10, "test_classic_insertion_sort.txt");
-//    res += sortion_test(watchdog_element_insertion_sort, not_req, 0, 150000, 5000, 10, "test_watchdog_element_insertion_sort.txt");
-//    res += sortion_test(binary_search_insertion_sort, not_req, 0, 150000, 5000, 10, "test_binary_search_insertion_sort.txt");
-//    res += sortion_test(classic_shell_sort, not_req, 0, 15000000, 500000, 10, "test_classic_shell_sort.txt");
-//    res += sortion_test(shell_sort, step_shell, 0, 15000000, 500000, 10, "test_step_shell.txt");
-//    res += sortion_test(shell_sort, step_frank_and_lazarus, 0, 15000000, 500000, 10, "test_step_frank_and_lazarus.txt");
-//    res += sortion_test(shell_sort, step_hibbard, 0, 15000000, 500000, 10, "test_step_hibbard.txt");
-//    res += sortion_test(shell_sort, step_papernov_and_stasivich, 0, 15000000, 500000, 10, "test_step_papernov_and_stasivich.txt");
-//    res += sortion_test(shell_sort, step_pratt, 0, 15000000, 500000, 10, "test_step_pratt.txt");
-//    res += sortion_test(shell_sort, step_knuth, 0, 15000000, 500000, 10, "test_step_knuth.txt");
-//    res += sortion_test(shell_sort, step_sedgewick_1, 0, 15000000, 500000, 10, "test_step_sedgewick_1.txt");
-//    res += sortion_test(shell_sort, step_sedgewick_2, 0, 15000000, 500000, 10, "test_step_sedgewick_2.txt");
-//    res += sortion_test(shell_sort, step_gonnet_and_baeza_yates, 0, 15000000, 500000, 10, "test_step_gonnet_and_baeza_yates.txt");
-//    res += sortion_test(shell_sort, step_tokuda, 0, 15000000, 500000, 10, "test_step_tokuda.txt");
-//    res += sortion_test(shell_sort, step_ciura, 0, 15000000, 500000, 10, "test_step_ciura.txt");
+//    res += sortion_test(classic_insertion_sort, not_req, 0, 150000, 5000, 5, "test_classic_insertion_sort.txt");
+//    res += sortion_test(watchdog_element_insertion_sort, not_req, 0, 150000, 5000, 5, "test_watchdog_element_insertion_sort.txt");
+//    res += sortion_test(binary_search_insertion_sort, not_req, 0, 150000, 5000, 5, "test_binary_search_insertion_sort.txt");
+//    res += sortion_test(classic_shell_sort, not_req, 0, 15000000, 500000, 5, "test_classic_shell_sort.txt");
+//    res += sortion_test(shell_sort, step_shell, 0, 15000000, 500000, 5, "test_step_shell.txt");
+//    res += sortion_test(shell_sort, step_frank_and_lazarus, 0, 15000000, 500000, 5, "test_step_frank_and_lazarus.txt");
+//    res += sortion_test(shell_sort, step_hibbard, 0, 15000000, 500000, 5, "test_step_hibbard.txt");
+//    res += sortion_test(shell_sort, step_papernov_and_stasivich, 0, 15000000, 500000, 5, "test_step_papernov_and_stasivich.txt");
+//    res += sortion_test(shell_sort, step_pratt, 0, 15000000, 500000, 5, "test_step_pratt.txt");
+//    res += sortion_test(shell_sort, step_knuth, 0, 15000000, 500000, 5, "test_step_knuth.txt");
+//    res += sortion_test(shell_sort, step_sedgewick_1, 0, 15000000, 500000, 5, "test_step_sedgewick_1.txt");
+//    res += sortion_test(shell_sort, step_sedgewick_2, 0, 15000000, 500000, 5, "test_step_sedgewick_2.txt");
+//    res += sortion_test(shell_sort, step_gonnet_and_baeza_yates, 0, 15000000, 500000, 5, "test_step_gonnet_and_baeza_yates.txt");
+//    res += sortion_test(shell_sort, step_tokuda, 0, 15000000, 500000, 5, "test_step_tokuda.txt");
+//    res += sortion_test(shell_sort, step_ciura, 0, 15000000, 500000, 5, "test_step_ciura.txt");
     return res;
 }
